6534733b4a20e768b1c00ad90690b34a4c8e9ae4
dmytro <lialin.d.v@gmail.com>
2024-04-28 23:41:14 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,15 @@
+
+const foo = 20;
+
+console.log('Test')
+
+if (foo !== 20) {
+
+    const bar = foo + 20;
+
+    if (bar > 30) {
+        // eslint-ignore-nextline
+        console.log(bar)
+    }
+}
+
